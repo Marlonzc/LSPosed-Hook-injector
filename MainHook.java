fc918d7e33dad8a2f67bad2f9b7a4f00d8317364
Marlonzc <79147244+Marlonzc@users.noreply.github.com>
2026-02-22 04:34:07 +0800

    Updated MainHook.java on 2026-02-21 20:34:03

---
+++ MainHook.java
@@ -0,0 +1,18 @@
+package com.loader.stealth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import de.robv.android.xposed.*;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedHelpers.findAndHookMethod(Activity.class, "onResume", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) {
+                FloatingPanel.show((Activity) param.thisObject);
+            }
+        });
+    }
+}
